3838d7effa301776af3eed3254e2f07d662bec4e
fnzmwor <96227809+Cakeds@users.noreply.github.com>
2022-01-06 17:47:38 +0900

    Create main.py

---
+++ 4강/main.py
@@ -0,0 +1,57 @@
+# # money = 99999999999999999999999999999999999999999999999999
+# # if money >= 100000000000000000000000000000000000000000000000000:
+# #     print("택시를 타고 가라 부자야")
+# # else:
+# #     print("걸어 가라 거지얏!")
+# # x = 3
+# # y = 999999999999999999999999999
+# # print(x > y)
+# # print(x<y)
+# # print(x==y)
+# # print(x!=y)
+# # if 3<5:
+# #     print("조건식이 True")
+# # if 3>5:
+# #     print("조건식이 False")
+# money = 10000
+# if money >= 3000:
+#     print("택시")
+# else:
+#     print("걸어")
+# rank = input('몇 등인가요(1 또는 2)?')
+# if rank == '1':
+#     print('쉬어!!!')
+# else:
+#     print('일해!!!!!!!!!')
+# number = 15
+# number1 = 16
+# if number % 3 == 0:
+#     print("{}는 3의 배수입니다.".format(number))
+# if number1 % 3 == 0:
+#     print("{}는 3의 배수입니다.".format(number))
+# else:
+#     print("{}는 3의 배수가 아닙니다.".format(number1))
+# money = 10000
+# distance = 100
+# rain = True
+# if money >= 10000 and distance >= 100 and rain == True:
+#     print("TAXI RIDE GOGO")
+# else:
+#     print("TAXI RIDE NONO")
+
+value = int(input("몇 학년인가요?(1~6)?"))
+#value = 5
+grade1 = 1
+grade2 = 2
+grade3 = 3
+grade4 = 4
+grade5 = 5
+grade6 = 6
+# if value >= grade2 and value <= grade4:
+#     print("KIMPAB EAT EAT")
+# else:
+#     print("HAMBURGER EAT EAT")
+if (value == grade1 or value == grade3 or value == grade5):
+    print("GO JIP HANSHI")
+else:
+    print("GO JIP DOOSHI")
