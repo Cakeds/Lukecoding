17c087cc7080a5d1f400bf619d27bfe40d4b3697
fnzmwor <96227809+Cakeds@users.noreply.github.com>
2022-01-27 17:50:37 +0900

    Create main.py

---
+++ 7강/main.py
@@ -0,0 +1,51 @@
+# prompt = """ 100. 종료 """
+# ㅜㅕㅡㅠㄷㄱ = 0
+# while ㅜㅕㅡㅠㄷㄱ != 100:
+#     print(prompt)
+#     ㅜㅕㅡㅠㄷㄱ = int(input())
+# print( 3 == 3) and (4 != 3)
+# jsdakkjadsfhkjasd
+# data = input("입력값:   ")
+# data1 = int(data) - 20
+# if data1 < 255 and data1 > 0:
+#     print(int(data) - 20)
+# elif int(data) < 0:
+#     print(0)
+# else:
+#     print(255)
+# number = 1
+# while number <= 10:
+#     print(number)
+#     number = number + 1
+# for number in range (3, 7, 1):
+#      print(number)
+# for number in range (3, 7, 1):
+#     print(number)
+#     print("------------------------------------------------------------------")
+# a = 0
+# while a < 1000:
+#     a = a + 1
+#     if a % 3 == 0:
+#         print(a)
+# aa = 3
+# for a in range(1, 10):
+#     aaa = '{} x {} = {}'.format(aa, a, aa * a)
+#     if a % 2 == 1:
+#         print(aaa)
+# a = 0
+# b = 0
+# while a < 10:
+#     a = a + 1
+#     b = a + b
+# print(b)
+# sum = 1
+# for i in range(1,11):
+#     sum = sum * i
+# print(sum)
+# score = int(input("님 시험 점수 망함 ㅋ: "))
+# while score != -1:
+#     if score > 60:
+#         print("안망")
+#     else:
+#         print("망")
+#     score = int(input("님 시험 점수 망함 ㅋ: "))
